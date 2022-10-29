a879881b5799e6b1a6a89a2317a1a4d14c69a199
Arabprogrammer <Arabprogrammer3030@gmail.com>
2022-10-29 10:52:53 +0200

    added Fraud Path to customer

--- Customer/src/main/java/com/customer/app/service/CustomerService.java
+++ Customer/src/main/java/com/customer/app/service/CustomerService.java
@@ -1,12 +1,23 @@
 package com.customer.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.customer.app.entity.Customer;
 import com.customer.app.repo.CustomerRepo;
+import com.fraud.app.controller.FraudCheckRespone;
+
+import lombok.AllArgsConstructor;
 
 @Service
-public record CustomerService(CustomerRepo customerRepo) {
+@AllArgsConstructor
+public class CustomerService {
+	
+	@Autowired
+	CustomerRepo customerRepo;
+	@Autowired
+	RestTemplate restTemplate;
 
 	public void registerCustomer(CustomerRegistrationRequest request) {
 //		TODO: check if Email valid
@@ -14,7 +25,13 @@ public record CustomerService(CustomerRepo customerRepo) {
 //		TODO: check if fraudster
 		Customer cus = Customer.builder().firstName(request.firstName()).lastName(request.lastName())
 				.email(request.email()).build();
-		customerRepo.save(cus);
+		customerRepo.saveAndFlush(cus);
+		String url = "http://localhost:8081/api/fraud/v1/fraud-check/{customerId}";
+		FraudCheckRespone fraudCheckRespone = restTemplate.getForObject
+				(url, FraudCheckRespone.class, cus.getId());
+		if(fraudCheckRespone.isFraudster()) {
+			throw new IllegalStateException("Fraudster");
+		}
 //		TODO: send notification
 	}
 
